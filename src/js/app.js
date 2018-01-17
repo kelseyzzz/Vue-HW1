d79331c477d5434db189d4d01bba54e4e7cf5d88
Kelsey Z <you@example.comzahandke@gmail.com>
2018-01-17 15:18:31 -0500

    added
    counter
    ux stuff

--- src/js/app.js
+++ src/js/app.js
@@ -3,26 +3,58 @@
 var app = new Vue({
 	el: '#app',
 	data: {
+		enteredName: '',
+		enteredEmail: '', 
 		people: [
 			{
 				name: 'Mickey Mouse',
-				email: 'mickeymouse@gmail.com'
+				email: 'mickeymouse@gmail.com',
+				time: Date.now(),
+				since: 0
 			},
 			{
 				name: 'Donald Duck',
-				email: 'quackD@gmail.com'
+				email: 'quackD@gmail.com',
+				time: Date.now(),
+				since: 0
 			},
 			{
 				name: 'Goofy',
-				email: 'goofy_all_the_time@gmail.com'
+				email: 'goofy_all_the_time@gmail.com',
+				time: Date.now(),
+				since: 0
 			},
 		],
-		enteredName: [],
-		enteredEmail: [], 
+	},
+	created: function() {
+		setInterval(this.updateTimeSinceValues, 3 * 1000)
+
+		
 	},
 	methods: {
-		storeEmail: function (event) {
-		console.log('IM WORKING');
+		updateTimeSinceValues: function() {
+			console.log('updateTimeSinceValue!')
+
+			this.people.forEach(person => {
+				person.since = Math.round((Date.now() - person.time)/1000/60);
+			});	
+		},
+		submitPerson: function (event) {
+			if (!this.enteredName || !this.enteredEmail) return;
+			console.log('IM WORKING');
+			
+			this.people.unshift({
+				  name: this.enteredName,
+				  email: this.enteredEmail,
+				  time: Date.now()
+			})
+
+			if (this.people.length > 4) {
+				this.people.pop();
+			}
+
+			this.enteredName = '';
+			this.enteredEmail = '';
 		}
 	  }	
 })
